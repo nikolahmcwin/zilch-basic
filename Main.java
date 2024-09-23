06e468c753851fd8b1d709c21e76351efcfa23e1
Nikolah McWin <nikolah.mcwin@gmail.com>
2024-09-24 09:04:58 +1000

    Main app class to handle user input and starting game

---
+++ Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+/*
+ * Base console app to run the game in
+ */
+public class Main {
+
+   public static void main(String[] args) {
+      
+      System.out.println("\n **** STARTING NEW GAME OF ZILCH ****");
+
+      boolean skipInput = false;
+      Game myFirstGame;
+       
+      if (skipInput) {
+         myFirstGame = new Game();
+      } else {
+         Scanner inputScanner = new Scanner(System.in);
+         int numPlayers = getNumPlayers(inputScanner);
+         String[] playerNames = getPlayerNames(inputScanner, numPlayers);
+         inputScanner.close();
+         myFirstGame = new Game(numPlayers, playerNames);
+      }
+
+      myFirstGame.startGame();
+      myFirstGame.printGameStatus();
+   }
+
+   // Use console input to get number of playeres
+   private static int getNumPlayers(Scanner input) {
+      int inNum = 0;
+      System.out.println("Enter number of players:");
+      while (inNum < 1 || inNum > 5) {
+         if (inNum != 0) {
+            System.out.println("Enter a number between 1 and 5.");
+         }
+         try {
+            inNum = Integer.parseInt(input.nextLine());
+         } catch (Exception e) {
+            System.out.println("Enter a valid number.");
+         }
+      }
+      return inNum;
+   }
+
+   // Use console input to get number of playeres
+   private static String[] getPlayerNames(Scanner input, int numPlayers) {
+      String[] playerNames = new String[numPlayers];
+      for (int i = 0; i < numPlayers; i++) {
+         System.out.println("Enter name for player " + Integer.toString(i + 1) + ":");
+         playerNames[i] = input.nextLine();
+      }
+      return playerNames;
+   }
+}
